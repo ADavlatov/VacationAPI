333c41a350a629d8bf7f1cbd275f56066c56f01b
ADavlatov <a@davlatov.space>
2022-09-26 21:43:22 +0300

    JWT Token

---
+++ VacationAPI/Queries/Authentication/Get/JwtToken.cs
@@ -0,0 +1,71 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using VacationAPI.Context;
+using VacationAPI.Entities;
+using VacationAPI.Services;
+
+namespace VacationAPI.Authentication;
+
+public class JwtToken
+{
+	public static IResult GetJwtToken(ApplicationContext db, string username, string password)
+	{
+		if (GetClaims(db, username, MD5Hash.GetHashedString(password)).Claims != null)
+		{
+			var response = new
+			{
+				access_token = new JwtSecurityTokenHandler().WriteToken(GenerateToken(db, username, MD5Hash.GetHashedString(password))),
+			};
+
+			return Results.Json(response);
+		}
+
+		return Results.Json("Неверный логин или пароль");
+	}
+
+	public static bool CheckJwtToken(string username, string jwtToken)
+	{
+		JwtSecurityToken jwt = new JwtSecurityToken(jwtToken);
+
+		if (username
+			== jwt.Claims.First()
+				.Value)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	private static JwtSecurityToken GenerateToken(ApplicationContext db, string username, string password)
+	{
+		return new(issuer: AuthOptions.ISSUER,
+			audience: AuthOptions.AUDIENCE,
+			claims: GetClaims(db, username, password)
+				.Claims,
+			expires: DateTime.UtcNow.Add(TimeSpan.FromHours(2)),
+			signingCredentials: new(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+	}
+
+	private static ClaimsIdentity GetClaims(ApplicationContext db, string username, string password)
+	{
+		User user = db.Users.FirstOrDefault(x => x.Name == username && x.Password == password);
+
+		if (user != null)
+		{
+			var claims = new List<Claim>
+			{
+				new(ClaimsIdentity.DefaultNameClaimType, user.Name)
+			};
+
+			ClaimsIdentity claimsIdentity =
+				new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+					ClaimsIdentity.DefaultRoleClaimType);
+
+			return claimsIdentity;
+		}
+
+		return null;
+	}
+}
