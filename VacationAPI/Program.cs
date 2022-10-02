0eb16fd35f8bf4ad0e375bd65ff1c877e0bc15a3
ADavlatov <a@davlatov.space>
2022-10-02 18:47:06 +0300

    Добавил валидаторы

--- VacationAPI/Program.cs
+++ VacationAPI/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using VacationAPI.Context;
+using VacationAPI.Entities;
+using VacationAPI.Request;
 using VacationAPI.Request.Authentication;
 using VacationAPI.Request.Authentication.Get;
+using VacationAPI.Validation;
+using VacationAPI.Validation.ValidationParameters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -44,9 +48,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//@TODO проверить все ли ошибки вводных данных учтены
 //@TODO добавить получение диаграмм со статистикой через https://github.com/image-charts/c-sharp
 //@TODO добавить возможность перемещать сотрудников из одной команды в другую
 //@TODO убрать employeePosition отовсюду
+
+RequestValidator _requestValidator;
+EmployeeValidator _employeeValidator;
+TeamValidator _teamValidator;
+DateValidator _dateValidator;
 
 //регистрация пользователя. Добавление в бд
 app.MapPost("/api/auth/sign-in/{username}/{password}", (ApplicationContext db, string username, string password) =>
@@ -64,14 +74,50 @@ app.MapGet("/api/auth/token/{username}/{password}", (ApplicationContext db, stri
 app.MapPost("api/teams/newteam/{teamName}/{username}/{access_token}",
 	(ApplicationContext db, string teamName, string username, string access_token) =>
 	{
-		return VacationAPI.Request.Teams.Post.NewTeam.AddNewTeam(db, teamName, username, access_token);
+		_requestValidator = new(db);
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		if (request.IsValid)
+		{
+			return VacationAPI.Request.Teams.Post.NewTeam.AddNewTeam(db, teamName, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //Получение списка всех команд пользователя
 app.MapGet("api/teams/{username}/{access_token}",
 	(ApplicationContext db, string username, string access_token) =>
 	{
-		return VacationAPI.Request.Teams.Get.Teams.GetTeams(db, username, access_token);
+		_requestValidator = new(db);
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		if (request.IsValid)
+		{
+			return VacationAPI.Request.Teams.Get.Teams.GetTeams(db, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //Получение диаграммы со статистикой всех команд (одна диаграмма для всех команд)
@@ -99,14 +145,64 @@ app.MapGet("api/teams/team/{teamName}/stats/{username}/{access_token}",
 app.MapPut("api/teams/team/{teamName}/{newTeamName}/{username}/{access_token}",
 	(ApplicationContext db, string teamName, string newTeamName, string username, string access_token) =>
 	{
-		return VacationAPI.Request.Teams.Put.TeamName.EditTeamName(db, teamName, newTeamName, username, access_token);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		if (request.IsValid && team.IsValid)
+		{
+			return VacationAPI.Request.Teams.Put.TeamName.EditTeamName(db, teamName, newTeamName, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //удаление команды
-app.MapDelete("api/teams/team/{teamName}/{username}/{accessToken}",
-	(ApplicationContext db, string teamName, string username, string accessToken) =>
+app.MapDelete("api/teams/team/{teamName}/{username}/{access_token}",
+	(ApplicationContext db, string teamName, string username, string access_token) =>
 	{
-		return VacationAPI.Request.Teams.Delete.Team.RemoveTeam(db, teamName, username, accessToken);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		if (request.IsValid && team.IsValid)
+		{
+			return VacationAPI.Request.Teams.Delete.Team.RemoveTeam(db, teamName, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //добавление работника
@@ -114,8 +210,33 @@ app.MapPost("api/teams/{teamName}/employees/newemployee/{employeeName}/{employee
 	(ApplicationContext db, string teamName, string employeeName, string employeePosition, string username,
 	string access_token) =>
 	{
-		return VacationAPI.Request.Employees.Post.Employee.AddNewEmployee(db, teamName, employeeName, employeePosition, username,
-			access_token);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		if (request.IsValid && team.IsValid)
+		{
+			return VacationAPI.Request.Employees.Post.Employee.AddNewEmployee(db, teamName, employeeName, employeePosition, username,
+				access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //получение списка работников команды
@@ -123,7 +244,32 @@ app.MapGet("api/teams/{teamName}/employees/{username}/{access_token}",
 	(ApplicationContext db, string teamName, string username,
 	string access_token) =>
 	{
-		return VacationAPI.Request.Employees.Get.Employees.GetEmployees(db, teamName, username, access_token);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		if (request.IsValid && team.IsValid)
+		{
+			return VacationAPI.Request.Employees.Get.Employees.GetEmployees(db, teamName, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //получение диаграмм со статистикой всех работников команды
@@ -147,17 +293,40 @@ app.MapPut("api/teams/{teamName}/employees/employee/{employeeName}/{newEmployeeN
 	(ApplicationContext db, string teamName, string employeeName, string newEmployeeName, string username,
 	string access_token) =>
 	{
-		return VacationAPI.Request.Employees.Put.EmployeeName.EditEmployeeName(db, teamName, employeeName, newEmployeeName, username,
-			access_token);
-	});
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+		_employeeValidator = new(db);
 
-//изменение должности работника
-app.MapPut("api/teams/{teamName}/employees/employee/{employeeName}/{newEmployeePosition}/{username}/{access_token}",
-	(ApplicationContext db, string teamName, string employeeName, string newEmployeePosition, string username,
-	string access_token) =>
-	{
-		return VacationAPI.Request.Employees.Put.EmployeePosition.EditEmployeePosition(db, teamName, employeeName, newEmployeePosition,
-			username, access_token);
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		var employee = _employeeValidator.Validate(new EmployeeNameParameter
+		{
+			EmployeeName = employeeName
+		});
+
+		if (request.IsValid && team.IsValid && employee.IsValid)
+		{
+			return VacationAPI.Request.Employees.Put.EmployeeName.EditEmployeeName(db, teamName, employeeName, newEmployeeName, username,
+				access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors),
+			employeeErrors = string.Join("\r\n", employee.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //удаление работника
@@ -165,7 +334,39 @@ app.MapDelete("api/teams/{teamName}/employees/employee/{employeeName}/{username}
 	(ApplicationContext db, string teamName, string employeeName, string username,
 	string access_token) =>
 	{
-		return VacationAPI.Request.Employees.Delete.Employee.RemoveEmployee(db, teamName, employeeName, username, access_token);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+		_employeeValidator = new(db);
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		var employee = _employeeValidator.Validate(new EmployeeNameParameter
+		{
+			EmployeeName = employeeName
+		});
+
+		if (request.IsValid && team.IsValid && employee.IsValid)
+		{
+			return VacationAPI.Request.Employees.Delete.Employee.RemoveEmployee(db, teamName, employeeName, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors),
+			employeeErrors = string.Join("\r\n", employee.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //Добавление отпуска сотруднику
@@ -175,8 +376,54 @@ app.MapPost(
 		string username,
 		string access_token) =>
 	{
-		return VacationAPI.Request.Vacations.Post.Vacation.AddNewVacation(db, teamName, employeeName, vacationDateStart, vacationDateEnd,
-			username, access_token);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+		_employeeValidator = new(db);
+		_dateValidator = new();
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		var employee = _employeeValidator.Validate(new EmployeeNameParameter
+		{
+			EmployeeName = employeeName
+		});
+
+		var dateStart = _dateValidator.Validate(new DateParameter
+		{
+			VacationDate = vacationDateStart
+		});
+
+		var dateEnd = _dateValidator.Validate(new DateParameter
+		{
+			VacationDate = vacationDateEnd
+		});
+
+		if (request.IsValid && team.IsValid && employee.IsValid && dateStart.IsValid && dateEnd.IsValid)
+		{
+			return VacationAPI.Request.Vacations.Post.Vacation.AddNewVacation(db, teamName, employeeName, vacationDateStart,
+				vacationDateEnd,
+				username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors),
+			employeeErrors = string.Join("\r\n", employee.Errors),
+			dateStartErrors = string.Join("\r\n", dateStart.Errors),
+			dateEndErrors = string.Join("\r\n", dateEnd.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //список отпусков работника
@@ -184,8 +431,40 @@ app.MapGet("api/teams/{teamName}/employees/{employeeName}/vacations/{username}/{
 	ApplicationContext db, string teamName, string employeeName, string vacationDateStart, string username,
 	string access_token) =>
 {
-	return VacationAPI.Request.Vacations.Get.Employee.GetVacations(db, teamName, employeeName, vacationDateStart, username,
-		access_token);
+	_requestValidator = new(db);
+	_teamValidator = new(db);
+	_employeeValidator = new(db);
+
+	var request = _requestValidator.Validate(new Request
+	{
+		Username = username,
+		AccessToken = access_token
+	});
+
+	var team = _teamValidator.Validate(new TeamNameParameter
+	{
+		TeamName = teamName
+	});
+
+	var employee = _employeeValidator.Validate(new EmployeeNameParameter
+	{
+		EmployeeName = employeeName
+	});
+
+	if (request.IsValid && team.IsValid && employee.IsValid)
+	{
+		return VacationAPI.Request.Vacations.Get.Employee.GetVacations(db, teamName, employeeName, vacationDateStart, username,
+			access_token);
+	}
+
+	var errorsResponse = new
+	{
+		requestErrors = string.Join("\r\n", request.Errors),
+		teamErrors = string.Join("\r\n", team.Errors),
+		employeeErrors = string.Join("\r\n", employee.Errors)
+	};
+
+	return Results.Json(errorsResponse);
 });
 
 //изменение начала отпуска сотрудника
@@ -195,8 +474,47 @@ app.MapPut(
 	string username,
 	string access_token) =>
 	{
-		return VacationAPI.Request.Vacations.Put.VacationStart.EditVacationStart(db, teamName, employeeName, vacationDateStart,
-			newVacationDateStart, username, access_token);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+		_employeeValidator = new(db);
+		_dateValidator = new();
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		var employee = _employeeValidator.Validate(new EmployeeNameParameter
+		{
+			EmployeeName = employeeName
+		});
+
+		var dateStart = _dateValidator.Validate(new DateParameter
+		{
+			VacationDate = vacationDateStart
+		});
+
+		if (request.IsValid && team.IsValid && employee.IsValid && dateStart.IsValid)
+		{
+			return VacationAPI.Request.Vacations.Put.VacationStart.EditVacationStart(db, teamName, employeeName, vacationDateStart,
+				newVacationDateStart, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors),
+			employeeErrors = string.Join("\r\n", employee.Errors),
+			dateStartErrors = string.Join("\r\n", dateStart.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //изменение конца отпуска сотрудника
@@ -206,18 +524,97 @@ app.MapPut(
 		string newVacationDateEnd, string username,
 		string access_token) =>
 	{
-		return VacationAPI.Request.Vacations.Put.VacationEnd.EditVacationEnd(db, teamName, employeeName, vacationDateEnd,
-			newVacationDateEnd, username, access_token);
+		_requestValidator = new(db);
+		_teamValidator = new(db);
+		_employeeValidator = new(db);
+		_dateValidator = new();
+
+		var request = _requestValidator.Validate(new Request
+		{
+			Username = username,
+			AccessToken = access_token
+		});
+
+		var team = _teamValidator.Validate(new TeamNameParameter
+		{
+			TeamName = teamName
+		});
+
+		var employee = _employeeValidator.Validate(new EmployeeNameParameter
+		{
+			EmployeeName = employeeName
+		});
+
+		var dateEnd = _dateValidator.Validate(new DateParameter
+		{
+			VacationDate = vacationDateEnd
+		});
+
+		if (request.IsValid && team.IsValid && employee.IsValid && dateEnd.IsValid)
+		{
+			return VacationAPI.Request.Vacations.Put.VacationEnd.EditVacationEnd(db, teamName, employeeName, vacationDateEnd,
+				newVacationDateEnd, username, access_token);
+		}
+
+		var errorsResponse = new
+		{
+			requestErrors = string.Join("\r\n", request.Errors),
+			teamErrors = string.Join("\r\n", team.Errors),
+			employeeErrors = string.Join("\r\n", employee.Errors),
+			dateStartErrors = string.Join("\r\n", dateEnd.Errors)
+		};
+
+		return Results.Json(errorsResponse);
 	});
 
 //удаление отпуска сотрудника
-app.MapDelete(
-	"api/teams/{teamName}/employees/{employeeName}/vacations/vacation/{vacationDateStart}/{vacationDateEnd}/{username}/{access_token}", (
-		ApplicationContext db, string teamName, string employeeName, string vacationDateStart, string username,
-		string access_token) =>
+app.MapDelete("api/teams/{teamName}/employees/{employeeName}/vacations/vacation/{vacationDateStart}/{username}/{access_token}", (
+	ApplicationContext db, string teamName, string employeeName, string vacationDateStart, string username,
+	string access_token) =>
+{
+	_requestValidator = new(db);
+	_teamValidator = new(db);
+	_employeeValidator = new(db);
+	_dateValidator = new();
+
+	var request = _requestValidator.Validate(new Request
+	{
+		Username = username,
+		AccessToken = access_token
+	});
+
+	var team = _teamValidator.Validate(new TeamNameParameter
+	{
+		TeamName = teamName
+	});
+
+	var employee = _employeeValidator.Validate(new EmployeeNameParameter
+	{
+		EmployeeName = employeeName
+	});
+
+	var dateStart = _dateValidator.Validate(new DateParameter
+	{
+		VacationDate = vacationDateStart
+	});
+
+	if (request.IsValid && team.IsValid && employee.IsValid && dateStart.IsValid)
 	{
 		return VacationAPI.Request.Vacations.Delete.Vacation.RemoveVacation(db, teamName, employeeName, vacationDateStart, username,
 			access_token);
-	});
+
+		;
+	}
+
+	var errorsResponse = new
+	{
+		requestErrors = string.Join("\r\n", request.Errors),
+		teamErrors = string.Join("\r\n", team.Errors),
+		employeeErrors = string.Join("\r\n", employee.Errors),
+		dateStartErrors = string.Join("\r\n", dateStart.Errors),
+	};
+
+	return Results.Json(errorsResponse);
+});
 
 app.Run();
